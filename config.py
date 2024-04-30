f69bbed6b16ed5bf62fdfbc7e705bcee78bb9b99
Alex Fogelson <fogelsonA@verizon.net>
2024-04-30 12:05:42 -0400

    Removed the config file from git index

--- config.py
+++
@@ -1,7 +0,0 @@
-MARKDOWN_FILES_PATH = './data/Markdown/'
-FOUNDATION_MODELS_PATH = './data/foundation_models.json'
-LLM_MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.2"
-LLM_MODEL_PATH = "./saved_models/mistral_model"
-LLM_TOKENIZER_PATH = "./saved_models/mistral_tokenizer"
-CITATION_MODEL_PATH = "allenai/multicite-multilabel-scibert"
-OPEN_ACCESS_PAPER_XREF = "data/open_access_paper_data.jsonl"
