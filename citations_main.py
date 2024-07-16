032dee1562a2e7ce3c6387e0a9f6a1410b2916ff
Alex Fogelson <fogelsonA@verizon.net>
2024-07-16 14:08:13 -0400

    Updates to the main citations script

--- citations_main.py
+++ citations_main.py
@@ -1,11 +1,8 @@
-from src.classifier.MultiCiteClassifier import MultiCiteClassifier
-from src.classifier.MistralEnhancedMulticiteClassifier import MistralEnhancedMulticiteClassifier
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from src.process.FoundationModel import FoundationModel
 from src.process.Corpus import Corpus
 from src.process.Cluster import Cluster
 from src.prompts.citation_prompts import PROMPT2
-
+from load_classifier import load_classifier
 
 import warnings, logging
 from datetime import datetime
@@ -13,7 +10,6 @@ import argparse
 from config import *
 from src.functional import extract_paper_metadata
 import pandas as pd
-from torch import cuda, backends, bfloat16
 
 logger = logging.getLogger(__name__)
 warnings.filterwarnings("ignore")
@@ -38,30 +34,8 @@ def main():
     resultsfile = f"results/citations/results_{right_now}_worker{args.index}of{args.workers}.csv"
     logging.basicConfig(filename=logfile, level=logging.DEBUG if args.debug else logging.INFO)
     
-    
-    device = 'mps' if backends.mps.is_available() else 'cuda:0' if cuda.is_available() else 'cpu'
-    print(f"Using device = {device}")
-    
-    
-    bnb_config = None if device.find('cuda') == 0 else BitsAndBytesConfig(load_in_4bit=True,
-                                    bnb_4bit_compute_dtype=bfloat16) 
-    refresh = False
-    try:
-        assert(not refresh)
-        model = AutoModelForCausalLM.from_pretrained(LLM_MODEL_PATH, device_map = device, quantization_config=bnb_config)
-        tokenizer = AutoTokenizer.from_pretrained(LLM_TOKENIZER_PATH, device = device)
-    except:
-        model = AutoModelForCausalLM.from_pretrained(LLM_MODEL_NAME, device_map=device, quantization_config=bnb_config)
-        tokenizer = AutoTokenizer.from_pretrained(LLM_MODEL_NAME, device = device)
-        model.save_pretrained(LLM_MODEL_PATH, from_pt=True)
-        tokenizer.save_pretrained(LLM_TOKENIZER_PATH, from_pt = True)
-  
-    classifier = MistralEnhancedMulticiteClassifier(model_checkpoint=CITATION_MODEL_PATH,
-                                                    llm_model=model,
-                                                    llm_tokenizer=tokenizer, 
-                                                    device=device, 
-                                                    prompt = PROMPT2,
-                                                    mc_uses_extends=True)
+    classifier = None #load_classifier(prompt = PROMPT2)
+
         
     corpus = Corpus(MARKDOWN_FILES_PATH, 
                         extensions = ['mmd'], 
@@ -74,8 +48,7 @@ def main():
     models = FoundationModel.modelsFromJSON(FOUNDATION_MODELS_PATH)
           
     corpus.findAllReferencesAllModels(models = models,
-                                     classifier =
-                                     classifier,
+                                     classifier = classifier,
                                      resultsfile = resultsfile)
 
         
